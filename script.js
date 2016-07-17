c9606e9391c9276cff54aa81fe0fb33b9e705150
daniel alroy <danielalroy@daniels-MacBook-Pro.local>
2016-07-17 17:55:21 +0300

    add exercises

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-var func1 = function func1() {
-	alert("Read More Books");
+
+/*var func1 = function() {
+	document.getElementById("t1").innerHTML = "Read More Books";
+}
+//document.getElementById("t1").onclick = func1;
+
+var func2 = function() {
+	document.getElementById("t2").innerHTML = "Daniel";
+}
+//document.getElementById("t2").onclick = func2;
+
+var toggle = function() {
+	
+	if (document.getElementById("t1").innerHTML === "Read More Books") {
+
+		document.getElementById("t1").innerHTML = "Welcome to our Book Store";
+
+
+
+	} else {
+
+		document.getElementById("t1").innerHTML = "Read More Books";
+	}
 }
 
-document.getElementById("t1").onclick = func1;
+document.getElementById("t1").onclick = toggle;*/
+document.getElementById("button").onclick = submitFrm;
+
+
+var submitFrm = function() {
+
+	alert("submit clicked");
+
+}
+
+	document.getElementById("button").onclick = submitFrm;
+
+
+
